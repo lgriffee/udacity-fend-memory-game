757232a71b9296e95f17b688d0383ed79b105c6a
Laura Griffee <laura@mailzone.com>
2018-06-19 14:25:22 -0600

    feat: "Open" cards when clicked on

--- js/app.js
+++ js/app.js
@@ -36,3 +36,12 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+//Flip over a card when it is clicked on
+const deck = document.querySelectorAll('.card');
+
+deck.forEach(function(card) {
+ card.addEventListener('click', function () {
+   card.classList.add('open', 'show');
+ });
+});
